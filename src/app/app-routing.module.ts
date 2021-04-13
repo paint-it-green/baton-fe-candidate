ce9aa926672bcc5f1fa49ae84f31fabfa53ff07c
paint-it-green <39176641+paint-it-green@users.noreply.github.com>
2021-04-13 23:19:22 +0800

    Revert "feat: add testing" (#4)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TodoListComponent } from './todo-list/todo-list.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/todo-list', pathMatch: 'full' },
-  {
-    path: 'todo-list',
-    component: TodoListComponent,
-    loadChildren: () =>
-      import('./todo-list/todo-list.module').then((m) => m.TodoListModule),
-  },
-];
-
-export const components = [
-  TodoListComponent
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
